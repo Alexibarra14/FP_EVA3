79d5a6f764e09437f95ba67c36893410b6807d16
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-11-27 13:21:15 -0600

    Add files via upload

---
+++ Eva3_10_arreglo5/src/eva3_10_arreglo5/Eva3_10_arreglo5.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_10_arreglo5;
+
+/**
+ *
+ * @author brend
+ */
+public class Eva3_10_arreglo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int[]n={1,2,3,4,5};
+       System.out.println(n.length);
+       String [][][][][] ciudades{{"CHIHUAHUA","JUAREZ","MADERA" }
+                                  {"DENVER","ANGELES","KANZAS" }
+                                  {"AGUASCALIENTES","CHILPANCINGO","COLORADO"}
+                                  { }}
+       
+    } 
+    
+    
+}
